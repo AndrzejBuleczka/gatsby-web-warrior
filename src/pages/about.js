64f27cda16781afd0716e3a89774c7bbf439f581
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-04-06 16:56:21 +0200

    adding navbar with Links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/NavBar'
 
 export default function about() {
   return (
     <div>
+      <Navbar />
       <h1>About</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, eveniet expedita ea, illo ducimus, nobis laborum veritatis cupiditate aspernatur ratione exercitationem sed? Non eos nobis cumque vero esse quo unde!</p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, eveniet expedita ea, illo ducimus, nobis laborum veritatis cupiditate aspernatur ratione exercitationem sed? Non eos nobis cumque vero esse quo unde!</p>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../components/NavBar'
 
 export default function Projects() {
   return (
     <div>
+      <Navbar />
       <h2>Portfolio</h2>
       <h3>Projects & Websites I've created</h3>
     </div>
