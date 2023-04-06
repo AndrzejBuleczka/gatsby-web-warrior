39e0689e8a898f26639522f4598a9ed202498b77
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-04-06 16:25:31 +0200

    adding 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sorry, that page doesn't exist</p>
+      <iframe 
+        width="453"
+        height="280"
+        src={`https://www.youtube.com/embed/t3otBjVZzT0`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  )
+}
