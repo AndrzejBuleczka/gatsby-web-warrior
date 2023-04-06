b563563a81e5b31f562fea435d181765b92634ab
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-04-06 16:11:16 +0200

    adding about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, eveniet expedita ea, illo ducimus, nobis laborum veritatis cupiditate aspernatur ratione exercitationem sed? Non eos nobis cumque vero esse quo unde!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, eveniet expedita ea, illo ducimus, nobis laborum veritatis cupiditate aspernatur ratione exercitationem sed? Non eos nobis cumque vero esse quo unde!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, eveniet expedita ea, illo ducimus, nobis laborum veritatis cupiditate aspernatur ratione exercitationem sed? Non eos nobis cumque vero esse quo unde!</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Web Warrior</div>
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop</h3>
+        <p>Front-end & web developer based in Kraków</p>
+      </div>
+    </section>
+  )
 }
