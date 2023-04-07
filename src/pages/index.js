cb02727be1f50c617d2a75cca245d6339bed0b32
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-04-07 11:16:37 +0200

    adding css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import * as React from "react"
-import Layout from "../components/Layout"
+import * as React from "react";
+import Layout from "../components/Layout";
+import * as styles from '../styles/home.module.css';
+import { Link } from "gatsby";
 
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop</h3>
           <p>Front-end & web developer based in Kraków</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
